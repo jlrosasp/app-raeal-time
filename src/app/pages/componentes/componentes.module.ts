4b7c828c7c8464b12967d0305ebca3404d3b9645
Jose Luis Rosas Peimbert <jlrosasp@ipera4.com>
2020-10-30 20:07:08 -0600

    Se habilita el sistema de rutas para nuestro proyecto y se concluye la funcionalidad del nuestro SideBar

---
+++ src/app/pages/componentes/componentes.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentesComponent } from './componentes.component';
+
+import { BotonesComponent } from './botones/botones.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { CancelacionComponent } from './cancelacion/cancelacion.component';
+
+import { ComponetesRouterModule } from './componentes.routes';
+
+@NgModule({
+  declarations: [
+    ComponentesComponent, 
+    BotonesComponent, 
+    TarjetasComponent, 
+    BusquedaComponent, 
+    CancelacionComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponetesRouterModule
+  ]
+})
+export class ComponentesModule { }
